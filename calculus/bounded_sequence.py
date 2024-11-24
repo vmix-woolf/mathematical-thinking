2413e7c4e9066e64e9981dbf8a5b6814538591c2
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-11-24 23:26:30 +0200

    add sequences

---
+++ calculus/bounded_sequence.py
@@ -0,0 +1,28 @@
+import random
+import matplotlib.pyplot as plt
+
+
+# Задаємо верхнє обмеження
+upper_limit = 10
+
+
+# Генеруємо множину випадкових елементів, обмежених зверху числом 10
+random_set = {random.randint(1, upper_limit) for _ in range(10)}
+# random_set = {random.randint(1, upper_limit)}
+
+
+# Виведення множини
+print("Множина випадкових елементів:", random_set)
+print("Обмеження зверху:", upper_limit)
+
+
+# Створення множини
+A = list(random_set)
+
+
+# Візуалізація засобами matplotlib
+plt.plot(A, [0] * len(A), 'ro') # 'ro' - червоні точки
+plt.title('Обмежена множина')
+plt.xlabel('Елементи множини')
+plt.yticks([]) # Вимкнення відображення осі y
+plt.show()
